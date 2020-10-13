399cdbb93e80b1083a30ebeec6b02c947aff4383
Nmtur <n.m.tureczek@gmail.com>
2020-10-13 11:32:25 +0200

    Update for Assignment

--- 5.KeaBriefcase/app.js
+++ 5.KeaBriefcase/app.js
@@ -1,6 +1,8 @@
 const  express = require("express");
 const fetch = require("node-fetch");
 const app = express();
+app.use(express.json()); // Disse to linjer er for at kunne bruge post
+app.use(express.urlencoded({extended: true})) // dette er for at kunne bruge url i upload.js
 
 app.use(express.static("public"));
 
@@ -8,6 +10,27 @@ app.use(express.static("public"));
 app.get("/", (req, res) =>{
     return res.sendfile( __dirname + "/public/upload/upload.html");
 });
+
+app.get("/about", (req, res) =>{
+    return res.sendfile( __dirname + "/public/about/about.html");
+});
+
+app.get("/download", (req, res) =>{
+    return res.sendfile( __dirname + "/public/download/download.html");
+});
+
+
+app.get("/form",(req, res) => {
+    console.log(req.query)
+    return res.send({data : req.query});
+});
+
+app.post("/form", (req, res) =>{
+    console.log(req.body);
+    return res.send({data : req.body});
+});
+
+
 
 
 //Konvension at listen ligger nederst
--- 5.KeaBriefcase/public/upload/upload.js
+++ 5.KeaBriefcase/public/upload/upload.js
@@ -1,1 +1,42 @@
-console.log("test");
+
+const currentYear = new Date().getFullYear();
+
+const footerCopyright = document.getElementById("footer-copyright");
+const copyrightElement = document.createTextNode("©" + currentYear);
+
+footerCopyright.appendChild(copyrightElement);
+
+
+// document.getElementById("form-submit").addEventListener("click", () =>{})
+
+function handleFormSubmitted(){
+    const formMessage = document.getElementById("messageBox").value;
+    console.log(formMessage);
+    $.get("/form?message="+formMessage, (response) =>{ //Kombinere flere med "^" eller "&"
+        console.log(response);
+    });
+}
+
+// change it to post this tile on /form
+function handleFormSubmittedPost() {
+    const formMessage = document.getElementById("messageBox").value;
+    console.log(formMessage);
+    $.post("/form?message=" + formMessage, (response) =>{
+        console.log(response);
+    });
+}
+
+function handleFormSubmittedPost2() {
+    const formMessage = document.getElementById("messageBox").value;
+    $.ajax({
+        url : "/form",
+        type : "POST",
+        data: {formMessage}
+    }).done((response) => {
+        console.log(response)
+    });
+}
+
+function validateForm() {
+    return true
+}

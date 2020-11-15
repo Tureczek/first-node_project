ed88db6b3cb4c00ba943f660ad23537a0c85311c
Nmtur <n.m.tureczek@gmail.com>
2020-11-15 21:01:34 +0100

    Authentication function added

--- Mandatorys/Tests/routes/subscribers.js
+++ Mandatorys/Tests/routes/subscribers.js
@@ -6,7 +6,9 @@ const bcrypt = require("bcrypt");
 
 // Getting all
 router.get("/", async (req, res) => {
+    console.log("Finding users")
     try {
+        console.log("Entering Try catch")
         const subscribers = await Subscriber.find();
         res.json(subscribers)
     } catch (err) {
@@ -22,11 +24,11 @@ router.get("/:id", getSubscriber, (req, res) => {
 // Creating One
 router.post("/", async (req, res) => {
 
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    //const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const subscriber = new Subscriber({
         name: req.body.name,
         subscribedToChannel: req.body.subscribedToChannel,
-        password: hashedPassword
+        password: req.body.password
     })
     try {
         const newSubscriber = await subscriber.save();
@@ -36,26 +38,15 @@ router.post("/", async (req, res) => {
     }
 });
 
-router.post("/login", async (req, res) => {
-   const user = Subscriber.findById(user => user.name === req.body.name)
-    if (user == null) {
-        return res.status(400).send("Cannot find user");
-    }
-    try{
-        if(await bcrypt.compare(req.body.password, user.password)){
-            res.send("Success");
-        } else {
-            res.send("Not allowed");
-        }
-    } catch (err){
-        res.status(500).send();
-    }
+
+router.post("/login", authenticate, (req, res) => {
+    res.json(res.subscriber)
 });
 
 
 // Updating One
 router.patch("/:id", getSubscriber, async (req, res) => {
-    if(req.body.name != null){
+    if (req.body.name != null) {
         res.subscriber.name = req.body.name
     }
     // if(req.body.name != null) {
@@ -64,7 +55,7 @@ router.patch("/:id", getSubscriber, async (req, res) => {
     try {
         const updatedSubscriber = await res.subscriber.save();
         res.json(updatedSubscriber)
-    } catch (err){
+    } catch (err) {
         res.status(400).json({ message: err.message })
     }
 });
@@ -73,24 +64,45 @@ router.patch("/:id", getSubscriber, async (req, res) => {
 router.delete("/:id", getSubscriber, async (req, res) => {
     try {
         await res.subscriber.remove()
-        res.json({ message:  "Deleted Subscriber" });
-    }catch (err) {f
+        res.json({message: "Deleted Subscriber"});
+    } catch (err) {
         res.status(500).json({ message: err.message })
     }
 });
 
 
 // Middleware
-async function getSubscriber(req, res, next){
+async function getSubscriber(req, res, next) {
     let subscriber
-    try{
+    try {
         subscriber = await Subscriber.findById(req.params.id)
-        if(subscriber == null){
-            return res.status(404).json({ message: "Cannot fins subscriber" })
+        if (subscriber == null) {
+            return res.status(404).json({ message: "Cannot find subscriber" })
         }
     } catch (err) {
         return res.status(500).json({ message: err.message })
     }
+    res.subscriber = subscriber
+    next()
+}
+
+
+async function authenticate(req, res, next) {
+    let subscriber;
+    let password;
+    subscriber = await Subscriber.find( { name: req.body.name, password: req.body.password } )
+    //subscriber = await Subscriber.find( subscriber => subscriber.name === req.body.name)
+    if (subscriber == null){
+        console.log("it is null ")
+        return res.status(400).json({ message: "Cannot find subscriber" })
+    }
+    try {
+
+    } catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
+    console.log("Found something")
+    console.log(subscriber)
     res.subscriber = subscriber
     next()
 }

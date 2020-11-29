48e62d9904881e167810011204820c3ea2441e43
Tureczek <n.m.tureczek@gmail.com>
2020-11-29 22:26:52 +0100

    socket_exercise connection between sockets

--- socket_exercise/app.js
+++ socket_exercise/app.js
@@ -1,24 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').createServer(app)
-const io = require("socket.io")(server)
 
-// Every time a user loads the website, it's going to call this function, which gives
-// each user it's own socket.
-io.on('connection', socket => {
-    //Send a message to the given user
-                // event name,   paramater / data
-    socket.emit('chat-message', 'Hello World')
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on("connection", (socket) => {
+    console.log(socket + 'A socket has connected');
+
+// make callback function
+    socket.on("client submit chat message", ({ data }) => {
+
+        socket.broadcast.emit("server send message", { data: data });
+    });
+
 });
+
+
+
 
 
 app.get("/", (req, res) => {
-    return res.send({ message: "OK"})
+    return res.sendFile(__dirname + "/chat.html")
 });
 
 
 
-app.listen(80, (error) =>{
-    if(error) throw new Error(error);
-    console.log(`server started on port 80`)
-});
+server.listen(8080, (error) =>{
+    if (error) throw new Error (error);
+    console.log(`server startet on port 8080`)
+})
+

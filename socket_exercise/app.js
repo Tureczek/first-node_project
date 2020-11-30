dc79cd5b25c4608225918f537f9540b15f2a2838
Tureczek <n.m.tureczek@gmail.com>
2020-11-30 11:10:48 +0100

    working chat application in socket_exercises

--- socket_exercise/app.js
+++ socket_exercise/app.js
@@ -3,7 +3,7 @@ const app = express();
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-
+/*
 io.on("connection", (socket) => {
     console.log(socket + 'A socket has connected');
 
@@ -14,14 +14,32 @@ io.on("connection", (socket) => {
     });
 
 });
+*/
 
+const users = {}
 
+io.on('connection', socket => {
+    socket.on('new-user', name => {
+        users[socket.id] = name
+        socket.broadcast.emit('user-connected', name)
+    })
+
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+    })
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
+    })
+})
 
 
 
 app.get("/", (req, res) => {
     return res.sendFile(__dirname + "/chat.html")
 });
+
 
 
 
